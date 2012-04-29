bc2afcb61422f8a7ec004360af33f400631907a8
Adrien Gallou <adriengallou@gmail.com>
2012-04-29 15:43:12 +0200

    use PHP_PATH instead of hardcoding php path in run.bat

--- data/run.bat
+++ data/run.bat
@@ -1,5 +1,7 @@
 @echo off
+REM  example: 
+REM  SET PHP_PATH="C:\Program Files\PHP\php.exe" && .\data\run.bat
 :start
-"C:\Program Files\PHP\php.exe" .\bin\auto-ih worker:run
+%PHP_PATH% .\bin\auto-ih worker:run
 ping localhost -n 20 -w 1000 > nul
 goto start

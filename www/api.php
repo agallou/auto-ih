2ad5f15d482f3baa5da24d3680b91d22f5443701
Adrien Gallou <adriengallou@gmail.com>
2012-04-29 14:09:26 +0200

    remove silex debug mode

--- www/api.php
+++ www/api.php
@@ -5,7 +5,6 @@ $loader->add('Autoih', 'src');
 $config        = new Autoih\Config\Config;
 $app           = new Silex\Application();
 $app['config'] = $config->load(__DIR__ . '/../config/configuration.yml');
-$app['debug']  = true;
 
 $app->mount('/genrsa', new Autoih\Provider\Controller\GenrsaController());
 $app->mount('/epmsi', new Autoih\Provider\Controller\EpmsiController());
